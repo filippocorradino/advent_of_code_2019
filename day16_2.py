c85a95214b39fe570e82c941592d3c5cb006e610
Filippo Corradino <filippo.corradino@gmail.com>
2020-11-28 21:09:14 +0100

    Improved Day 16 part 2

--- day16_2.py
+++ day16_2.py
@@ -14,28 +14,28 @@ __author__ = "Filippo Corradino"
 __email__ = "filippo.corradino@gmail.com"
 
 
-def cheaty_fft_phase(data, start_index):
+def cheaty_fft_phase(data):
     output = data.copy()
-    total = sum(data[start_index-1:])
-    for k in range(start_index, len(data)):
-        total = total - data[k-1]
+    total = sum(data)
+    for k in range(len(data)):
         output[k] = abs(total) % 10
+        total = total - data[k]
     return output
 
 
-def main(n_phases=100, repeat=1000, message_len=8, offset_indicator_len=7):
+def main(n_phases=100, repeat=10000, message_len=8, offset_indicator_len=7):
     data = []
     with open('inputs/day_16_input.txt') as file:
         for digit in file.read():
             data.append(int(digit))
-    data = data*10000
+    data = data * repeat
     start_index = int(''.join(str(x) for x in data[:offset_indicator_len]))
     if start_index < (len(data) // 2) or start_index + message_len > len(data):
         raise RuntimeError("I surrender!")
+    data = data[start_index:]
     for k in range(n_phases):
-        data = cheaty_fft_phase(data, start_index)
-    end_index = start_index + message_len
-    datastring = ''.join(str(x) for x in data[start_index:end_index])
+        data = cheaty_fft_phase(data)
+    datastring = ''.join(str(x) for x in data[:message_len])
     print("\nMessage extracted after {0} FFT phases: {1}\n"
           .format(n_phases, datastring))
     return int(datastring)
